3e7c2723dba86b72d93068f0c0366ceb5d0445ff
josh-merrell <jmerrell93@hotmail.com>
2022-03-16 20:51:39 -0600

    solo_03162022_01

---
+++ build/bundle.js
@@ -0,0 +1,32 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./client/index.js":
+/*!*************************!*\
+  !*** ./client/index.js ***!
+  \*************************/
+/***/ (() => {
+
+eval("throw new Error(\"Module build failed (from ./node_modules/babel-loader/lib/index.js):\\nError [ERR_MODULE_NOT_FOUND]: Cannot find package '@babel/preset-react' imported from /home/jmerrell93/Codesmith/solo/babel-virtual-resolve-base.js\\n    at new NodeError (/home/jmerrell93/Codesmith/solo/node_modules/@babel/core/lib/vendor/import-meta-resolve.js:2552:5)\\n    at packageResolve (/home/jmerrell93/Codesmith/solo/node_modules/@babel/core/lib/vendor/import-meta-resolve.js:3208:9)\\n    at moduleResolve (/home/jmerrell93/Codesmith/solo/node_modules/@babel/core/lib/vendor/import-meta-resolve.js:3242:18)\\n    at defaultResolve (/home/jmerrell93/Codesmith/solo/node_modules/@babel/core/lib/vendor/import-meta-resolve.js:3281:13)\\n    at /home/jmerrell93/Codesmith/solo/node_modules/@babel/core/lib/vendor/import-meta-resolve.js:3304:14\\n    at Generator.next (<anonymous>)\\n    at asyncGeneratorStep (/home/jmerrell93/Codesmith/solo/node_modules/@babel/core/lib/vendor/import-meta-resolve.js:63:103)\\n    at _next (/home/jmerrell93/Codesmith/solo/node_modules/@babel/core/lib/vendor/import-meta-resolve.js:65:194)\\n    at /home/jmerrell93/Codesmith/solo/node_modules/@babel/core/lib/vendor/import-meta-resolve.js:65:364\\n    at new Promise (<anonymous>)\");\n\n//# sourceURL=webpack://shortest-path-finder/./client/index.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./client/index.js"]();
+/******/ 	
+/******/ })()
+;
---
+++ client/App.jsx
@@ -0,0 +1,30 @@
+/**
+ * ************************************
+ *
+ * @module  App.jsx
+ * @author
+ * @date
+ * @description
+ *
+ * ************************************
+ */
+
+ import React, { Component } from 'react';
+ import MainDisplay from './containers/MainDisplay.jsx';
+ 
+ class App extends Component {
+   constructor(props) {
+     super(props);
+   }
+ 
+   render() {
+     return(
+       <div>
+         <MainDisplay/>
+       </div>
+     );
+   }
+ }
+ 
+ export default App;
+ 
